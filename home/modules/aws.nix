c5839a479074ad95cc68dcc0bf9a63dab55feb06
Willi Carlsen <carlsenwilli@gmail.com>
2023-10-23 13:02:51 +0200

    Fix granted with correct alias
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- home/modules/aws.nix
+++ home/modules/aws.nix
@@ -1,19 +1,6 @@
 {pkgs, unstable, system, ... }:
 let
-  unstablePkgs = import unstable { overlays = [
-    (final: prev:
-      {
-        granted = prev.granted.overrideAttrs (old: rec {
-          pname = "granted";
-          version = "1.19.1";
-          src = prev.fetchFromGitHub {
-            owner = "common-fate";
-            repo = pname;
-            rev = "v${version}";
-            sha256 = "sha256-BvrMfQ/fiAMJCROwOqzt17ae/qqDC2KFdBK2epVImus=";
-          };
-        });
-      })]; inherit system;};
+  unstablePkgs = import unstable { inherit system; };
 in
 {
   home.packages = with pkgs; [
@@ -23,12 +10,13 @@ in
     (unstablePkgs.granted)
   ];
 
-  # programs.zsh = {
-  #   sessionVariables = {
-  #     GRANTED_ALIAS_CONFIGURED = "true";
-  #   };
-  #   envExtra = ''
-  #     alias assume="source assume"
-  #   '';
-  # };
+  programs.zsh = {
+    sessionVariables = {
+      GRANTED_ALIAS_CONFIGURED = "true";
+      GRANTED_ENABLE_AUTO_REASSUME=true;
+    };
+    envExtra = ''
+      alias assume="source ${unstablePkgs.granted}/bin/.assume-wrapped"
+    '';
+  };
 }
